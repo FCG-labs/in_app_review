7fdb60d57061ae0c9fbcabf78672e0d3bad5bbbf
FCG <143424305+FCG-labs@users.noreply.github.com>
2025-05-26 11:12:23 +0900

    -

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,7 +3,7 @@ export interface OpenStoreOptions {
     microsoftStoreId?: string;
 }
 export declare function isAvailable(): Promise<boolean>;
-export declare function requestReview(): Promise<void>;
+export declare function requestReview(isFakeMode?: boolean): Promise<void>;
 export declare function openStoreListing(options?: OpenStoreOptions): Promise<void>;
 declare const _default: {
     isAvailable: typeof isAvailable;
